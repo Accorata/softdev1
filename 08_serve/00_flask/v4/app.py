e3d9b889a24c88711999deaf9eeaae008788871f
Henry Bach <henrypbach@gmail.com>
2022-10-06 14:03:31 -0400

    08 stuff

--- 08_serve/00_flask/v4/app.py
+++ 08_serve/00_flask/v4/app.py
@@ -1,6 +1,12 @@
-# Shy : Sasha S, Henry B, Yuki F
-# SoftDev
-# Oct 2022
+'''
+Shy : Sasha S, Henry B, Yuki F
+SoftDev
+Oct 2022
+DISCO :
+    Debugger works
+    Recognizes when you change the code
+    Interpreted as HTML 
+'''
 
 from flask import Flask
 app = Flask(__name__) #create instance of class Flask
@@ -9,7 +15,7 @@ app = Flask(__name__) #create instance of class Flask
 def hello_world():
     print("the __name__ of this module is... ")
     print(__name__)
-    return "No hablo queso!"
+    return "<h1>No hablo queso!</h1>5tulk"
 
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
