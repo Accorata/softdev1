360c001c5760e9aefe13d453deab4a14de4d7305
Henry Bach <henrypbach@gmail.com>
2022-11-06 21:01:05 -0500

    redirect added

---
+++ 19_session/app.py
@@ -0,0 +1,41 @@
+'''
+Henali: Erica (hugo), Henry (e), Aahan (spikes)
+19_Session
+SoftDev
+2022-11-03
+time spent: 0.5 hr
+
+DISCO:
+QCC:
+
+'''
+from flask import Flask, session, request, render_template, redirect, url_for
+
+app = Flask(__name__)
+app.secret_key = 'foo'
+
+@app.route("/") #, methods = ['POST'])
+def login():
+    if 'username' in session:
+        return render_template('welcome.html')
+    return render_template('login.html')
+
+@app.route("/welcome", methods = ['POST'])
+def welcome():
+    if request.form.get('username') == "Foo" and request.form.get('password') == "Bar" :
+        session['username'] = request.form.get('username')
+        return render_template('welcome.html', username=session['username'])
+    return redirect(url_for('login'))
+
+@app.route("/logout", methods = ['POST'])
+def logout():
+    session.pop('username')
+    return redirect(url_for('login'))
+
+# @app.route("/test")
+# def test():
+#     return "test"
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
