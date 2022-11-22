396d4f94874b4b60c487362e5cb62b6c9e160cdf
Henry Bach <henrypbach@gmail.com>
2022-11-22 13:36:43 -0500

    grabbed teamate's + team name + readme

--- 20_restapi/app.py
+++ 20_restapi/app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
+import urllib.request, json
 
 app = Flask(__name__)
 
 @app.route("/")
-def load_main () :
-    return render_template("main.html", test="hello")
+def load_main():
+    key = open("key_nasa.txt", "r").read()
+    url = "https://api.nasa.gov/planetary/apod?api_key=" + key
+
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    dict = json.loads(data)
+    return render_template("main.html", image1=dict["hdurl"], desc=dict["explanation"])
 
 
 if __name__ == "__main__":
